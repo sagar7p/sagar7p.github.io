62fe013fad325f473c691a7343ae0c096e3da062
SagarPunhani <sagar7p@gmail.com>
2014-12-24 22:23:53 -0600

    Check 2

--- send.php
+++ send.php
@@ -1,15 +1,6 @@
 <?php
-/*
 $name = $_POST['name'];
-$email = $_POST['email'];
-$comment =$_POST['comment'];
+$eamil = $_POST['email'];
+$message = $_POST['message'];
 
-$to = "sagar7p@gmail.com"
-$subject = "Contact Form";
-$body = "Name: $name \n Email: $email \n Message: $comment";
-
-mail($to,$subject,$body);
-
-header: header("location: contact.html");
-*/
 ?>
