c6421251cbb8c329dc8d94da75f461fef3476d26
SagarPunhani <sagar7p@gmail.com>
2014-12-24 22:19:45 -0600

    Check

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $name = $_POST['name'];
 $email = $_POST['email'];
 $comment =$_POST['comment'];
@@ -9,10 +10,6 @@ $body = "Name: $name \n Email: $email \n Message: $comment";
 
 mail($to,$subject,$body);
 
-header: header("location: contact.html");/*if(isset($_POST['submit'])) {
-	$msg = 'Name: ' .$_POST['name'] ."\n"
-			.'Email: ' .$_POST['email'] ."\n"
-			.'Message: ' .$_POST['comment'] ."\n"
-	mail('sagar7p@gmail.com','Contact Form', $msg);*/
-}
+header: header("location: contact.html");
+*/
 ?>
