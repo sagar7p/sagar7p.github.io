dfa9eba6d8105360f9ccf30945ea7d1f43def819
SagarPunhani <sagar7p@gmail.com>
2014-12-24 21:57:17 -0600

    Contact Page

---
+++ send.php
@@ -0,0 +1,7 @@
+<?php
+if(isset($_POST['submit'])) {
+	$msg = 'Name: ' .$_POST['name'] ."\n"
+			.'Email: ' .$_POST['email'] ."\n"
+			.'Message: ' .$_POST['comment'] ."\n"
+	mail('sagar7p@gmail.com','Contact Form', $msg);
+}
