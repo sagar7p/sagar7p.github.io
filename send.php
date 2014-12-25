750abd4b07d3ca722d5b37d5b5d793f95633e5d3
SagarPunhani <sagar7p@gmail.com>
2014-12-24 22:11:44 -0600

    New Mail

--- send.php
+++ send.php
@@ -1,8 +1,19 @@
 <?php
-if(isset($_POST['submit'])) {
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment =$_POST['comment'];
+
+$to = "sagar7p@gmail.com"
+$subject = "Contact Form";
+$body = "Name: $name \n Email: $email \n Message: $comment";
+
+mail($to,$subject,$body);
+
+echo "Message Sent! <a href='contact.html'>Click Here</a> to send another email";
+/*if(isset($_POST['submit'])) {
 	$msg = 'Name: ' .$_POST['name'] ."\n"
 			.'Email: ' .$_POST['email'] ."\n"
 			.'Message: ' .$_POST['comment'] ."\n"
-	mail('sagar7p@gmail.com','Contact Form', $msg);
+	mail('sagar7p@gmail.com','Contact Form', $msg);*/
 }
 ?>
